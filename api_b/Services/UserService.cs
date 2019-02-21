f104590d181904f267577625157825a8765ed200
jeff <jeanfrancoisbouchard3@gmail.com>
2019-02-21 11:00:44 -0500

    Duplication des BD

--- api_b/Services/UserService.cs
+++ api_b/Services/UserService.cs
@@ -9,12 +9,18 @@ namespace api_b.Services
     public class UserService
     {
         private readonly IMongoCollection<User> _users;
+        private readonly IMongoCollection<User> _usersP;
 
         public UserService(IConfiguration config)
         {
             var client = new MongoClient(config.GetConnectionString("tp4"));
             var database = client.GetDatabase("tp4");
             _users = database.GetCollection<User>("users");
+
+            var clientP = new MongoClient(config.GetConnectionString("tp4p"));
+            var databaseP = clientP.GetDatabase("tp4");
+            _usersP = databaseP.GetCollection<User>("users");
+
         }
 
         public List<User> Get()
@@ -30,6 +36,7 @@ namespace api_b.Services
         public User Create(User user)
         {
             _users.InsertOne(user);
+            _usersP.InsertOne(user);
             return user;
         }
     }
